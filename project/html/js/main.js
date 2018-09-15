6c73978dc5c8acaec1e9d4f05850ca444eb2212d
Lukas Elmer <lukas.elmer@gmail.com>
2018-09-15 17:33:58 +0200

    rewrite js

--- project/html/js/main.js
+++ project/html/js/main.js
@@ -1,47 +1,52 @@
-var application = function(){
-    var ALMemory = null;
-	
-    var log = function(l){
-        if(console) console.log(l)  ;  
-    };
-    
-    $("#touch_me").click(function(){
-        $("#touch_me").fadeOut(function(){
-            setTimeout(function(){
-                $("#touch_me").fadeIn()
-            }, 3000)
-        })
-    })
-    
-    function changeBGColor(data){
-        bg_animation = data[2];
-        bg_color1 = data[0];
-        bg_color2 = null;
-        bg_duration = data[1];
-        $("body").animate({ "background-color": bg_color1 }, bg_duration, bg_animation);
-    }
-	
-	/*QiSession Events*/
-	 
-    var onConnected = function(session){
-        log("connected");
-        session.service("ALMemory").then(function (serv) {
-            ALMemory = serv;
-        },
-        function(error){
-            log("Unable to get the service ALMemory : " + error);
-        });
-        RobotUtils.subscribeToALMemoryEvent("template/changeBGColor", changeBGColor);
-    };
-	
-    var onError = function(){
-        log("Disconnected, or failed to connect :-(");
-    };
-        
-    var init = function(){
-        RobotUtils.connect(onConnected, onError); // async !
-        return this;
-    };
-    
-    return init();
+const application = function() {
+  let ALMemory = null;
+
+  const log = function(l) {
+    if (console) console.log(l);
+  };
+
+  $('#touch_me').click(() => {
+    $('#touch_me').fadeOut(() => {
+      setTimeout(() => {
+        $('#touch_me').fadeIn();
+      }, 3000);
+    });
+  });
+
+  function changeBGColor(data) {
+    const bg_animation = data[2];
+    const bg_color1 = data[0];
+    const bg_color2 = null;
+    const bg_duration = data[1];
+    $('body').animate({ 'background-color': bg_color1 }, bg_duration, bg_animation);
+  }
+
+  /* QiSession Events*/
+
+  const onConnected = function(session) {
+    log('connected');
+    session.service('ALMemory').then(
+      (serv) => {
+        ALMemory = serv;
+      },
+      (error) => {
+        log(`Unable to get the service ALMemory : ${ error}`);
+      }
+    );
+    RobotUtils.subscribeToALMemoryEvent('template/changeBGColor', changeBGColor);
+  };
+
+  const onError = function() {
+    log('Disconnected, or failed to connect :-(');
+  };
+
+  const init = function() {
+    RobotUtils.connect(
+      onConnected,
+      onError
+    ); // async !
+    return this;
+  };
+
+  return init();
 };
